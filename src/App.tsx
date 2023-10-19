a8de23587c350449301ccf37457b60211386d43d
Saykho <saikho@tpu.ru>
2023-10-19 16:06:04 +0700

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { Main } from './pages/main';
 import { PlacesCount } from './const/placesCount.ts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoute } from './const/settings.ts';
+import { Login } from './pages/login';
+import { Offer } from './pages/offer';
+import { Favorites } from './pages/favorites';
+import { Error } from './components/error';
 
 export default function App() {
   return (
-    <Main placesCount={PlacesCount.count}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<Main placesCount={PlacesCount.count}/>}/>
+        <Route path={AppRoute.Login} element={<Login/>}/>
+        <Route path={AppRoute.Offer} element={<Offer/>}/>
+        <Route path={AppRoute.Favorites} element={<Favorites/>}/>
+        <Route path='*' element={<Error/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
