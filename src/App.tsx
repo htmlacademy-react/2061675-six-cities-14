9b26693dcefb13f79d411377f520d201db410029
Saykho <saikho@tpu.ru>
2023-10-19 16:34:05 +0700

    redirect the user to the Login page from the Favorites page if he is not authorized

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Main } from './pages/main';
 import { PlacesCount } from './const/placesCount.ts';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AppRoute } from './const/settings.ts';
+import { AppRoute, AuthorizationStatus } from './const/settings.ts';
 import { Login } from './pages/login';
 import { Offer } from './pages/offer';
 import { Favorites } from './pages/favorites';
 import { Error } from './components/error';
+import { PrivateRoute } from './components/privateRoute';
 
 export default function App() {
   return (
@@ -14,7 +15,7 @@ export default function App() {
         <Route path={AppRoute.Main} element={<Main placesCount={PlacesCount.count}/>}/>
         <Route path={AppRoute.Login} element={<Login/>}/>
         <Route path={AppRoute.Offer} element={<Offer/>}/>
-        <Route path={AppRoute.Favorites} element={<Favorites/>}/>
+        <Route path={AppRoute.Favorites} element={<PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}><Favorites/></PrivateRoute>}></Route>
         <Route path='*' element={<Error/>}/>
       </Routes>
     </BrowserRouter>
