51c37aa55c2a7e65e71c2cd096a8dfebf8fe7d81
Saykho <saikho@tpu.ru>
2023-10-19 12:01:20 +0700

    create component FavoriteCard, page Favorites

--- src/components/favoriteCard/FavoriteCard.tsx
+++ src/components/favoriteCard/FavoriteCard.tsx
@@ -1,36 +1,28 @@
 import React from 'react';
+import { CardProps } from '../card';
 
-export type CardProps = {
-  premium: string;
-  imageSrc: string;
-  price: number;
-  placeName: string;
-  placeType: string;
-  ratingStars: string;
-}
-
-export const Card: React.FC<CardProps> = ({imageSrc, placeName, placeType, price, premium, ratingStars}) => {
+export const FavoriteCard: React.FC<CardProps> = ({ratingStars, placeType, placeName, price, imageSrc, premium}) => {
   return (
-    <article className="cities__card place-card">
+    <article className="favorites__card place-card">
       <div className="place-card__mark">
         <span>{premium}</span>
       </div>
-      <div className="cities__image-wrapper place-card__image-wrapper">
+      <div className="favorites__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src={imageSrc} width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src={imageSrc} width="150" height="110" alt="Place image"/>
         </a>
       </div>
-      <div className="place-card__info">
+      <div className="favorites__card-info place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
             <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">To bookmarks</span>
+            <span className="visually-hidden">In bookmarks</span>
           </button>
         </div>
         <div className="place-card__rating rating">
