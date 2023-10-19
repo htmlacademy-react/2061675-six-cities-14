cc622255328a270b4f66c0f8c52f25b1c58792c3
Saykho <saikho@tpu.ru>
2023-10-19 15:04:01 +0700

    reset commits

--- src/components/notFound/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-export const NotFound: React.FC = () => (
-  <div>
-    404 Not Found
-    <Link to="/">Вернуться на главную страницу</Link>
-  </div>
-);
