5d211efd40c5e259b981abc947b74342dab85565
ssar_say <sayhoo.saryglar@mail.ru>
2023-10-30 17:57:31 +0700

    add sorting logic

--- src/components/offersList/OffersList.tsx
+++ src/components/offersList/OffersList.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { MockOffers } from '../../mocks';
 import { Card } from '../card';
+import { Offer } from '../../types/offer.ts';
 
-export const OffersList: React.FC = () => (
-  <div className="cities__places-list places__list tabs__content">
-    {MockOffers.map((offer) => (
-      <Card offer={offer} key={offer.id}/>
-    ))}
-  </div>
-);
+export const OffersList: React.FC<{ sortOption: string }> = ({sortOption}) => {
+  const getSortedOffers = (): Offer[] => {
+    switch (sortOption) {
+      case 'PriceLowToHigh':
+        return MockOffers.slice().sort((a, b) => a.price - b.price);
+      case 'PriceHighToLow':
+        return MockOffers.slice().sort((a, b) => b.price - a.price);
+      case 'TopRatedFirst':
+        return MockOffers.slice().sort((a, b) => b.rating - a.rating);
+      default:
+        return MockOffers;
+    }
+  };
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {getSortedOffers().map((offer) => (
+        <Card offer={offer} key={offer.id} />
+      ))}
+    </div>
+  );
+};
--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../../components/header';
 import { Location } from '../../components/location';
 import { MockOffers } from '../../mocks';
@@ -8,67 +8,73 @@ import { OffersList } from '../../components/offersList';
 type MainProps = {
   placesCount: number;
 }
-export const Main: React.FC<MainProps> = ({placesCount}) => (
-  <div className="page page--gray page--main">
-    <Header/>
-    <main className="page__main page__main--index">
-      <h1 className="visually-hidden">Cities</h1>
-      <div className="tabs">
-        <section className="locations container">
-          <ul className="locations__list tabs__list">
-            <li className="locations__item">
-              <Location locationName="Paris" active=""/>
-            </li>
-            <li className="locations__item">
-              <Location locationName="Cologne" active=""/>
-            </li>
-            <li className="locations__item">
-              <Location locationName="Brussels" active=""/>
-            </li>
-            <li className="locations__item">
-              <Location locationName="Amsterdam" active="tabs__item--active"/>
-            </li>
-            <li className="locations__item">
-              <Location locationName="Hamburg" active=""/>
-            </li>
-            <li className="locations__item">
-              <Location locationName="Dusseldorf" active=""/>
-            </li>
-          </ul>
-        </section>
-      </div>
-      <div className="cities">
-        <div className="cities__places-container container">
-          {
-            MockOffers.length ?
-              <>
-                <section className="cities__places places">
-                  <h2 className="visually-hidden">Places</h2>
-                  <b className="places__found">{placesCount} places to stay in Amsterdam</b>
-                  <form className="places__sorting" action="#" method="get">
-                    <span className="places__sorting-caption">Sort by</span>
-                    <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                      <svg className="places__sorting-arrow" width="7" height="4">
-                        <use xlinkHref="#icon-arrow-select"></use>
-                      </svg>
-                    </span>
-                    <ul className="places__options places__options--custom places__options--opened">
-                      <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                      <li className="places__option" tabIndex={0}>Price: low to high</li>
-                      <li className="places__option" tabIndex={0}>Price: high to low</li>
-                      <li className="places__option" tabIndex={0}>Top rated first</li>
-                    </ul>
-                  </form>
-                  <OffersList/>
-                </section>
-                <div className="cities__right-section">
-                  <section className="cities__map map"></section>
-                </div>
-              </> : <Empty/>
-          }
+export const Main: React.FC<MainProps> = ({placesCount}) => {
+  const [sortOption, setSortOption] = useState('Popular');
+
+  const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSortOption(event.target.value);
+  };
+
+  return (
+    <div className="page page--gray page--main">
+      <Header/>
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <div className="tabs">
+          <section className="locations container">
+            <ul className="locations__list tabs__list">
+              <li className="locations__item">
+                <Location locationName="Paris" active=""/>
+              </li>
+              <li className="locations__item">
+                <Location locationName="Cologne" active=""/>
+              </li>
+              <li className="locations__item">
+                <Location locationName="Brussels" active=""/>
+              </li>
+              <li className="locations__item">
+                <Location locationName="Amsterdam" active="tabs__item--active"/>
+              </li>
+              <li className="locations__item">
+                <Location locationName="Hamburg" active=""/>
+              </li>
+              <li className="locations__item">
+                <Location locationName="Dusseldorf" active=""/>
+              </li>
+            </ul>
+          </section>
         </div>
-      </div>
-    </main>
-  </div>
-);
+        <div className="cities">
+          <div className="cities__places-container container">
+            {
+              MockOffers.length ?
+                <>
+                  <section className="cities__places places">
+                    <h2 className="visually-hidden">Places</h2>
+                    <b className="places__found">{placesCount} places to stay in Amsterdam</b>
+                    <form className="places__sorting" action="#" method="get">
+                      <span className="places__sorting-caption">Sort by</span>
+                      <select
+                        className="places__sorting-type"
+                        value={sortOption}
+                        onChange={handleSortChange}
+                      >
+                        <option value="Popular">Popular</option>
+                        <option value="PriceLowToHigh">Price: low to high</option>
+                        <option value="PriceHighToLow">Price: high to low</option>
+                        <option value="TopRatedFirst">Top rated first</option>
+                      </select>
+                    </form>
+                    <OffersList sortOption={sortOption}/>
+                  </section>
+                  <div className="cities__right-section">
+                    <section className="cities__map map"></section>
+                  </div>
+                </> : <Empty/>
+            }
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
