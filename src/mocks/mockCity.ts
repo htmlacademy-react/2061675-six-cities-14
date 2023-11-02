aa626db1d2ed823dfb9da6d675116dc2d418b477
Saykho <saikho@tpu.ru>
2023-11-02 11:35:53 +0700

    add Amsterdam city

---
+++ src/mocks/mockCity.ts
@@ -0,0 +1,10 @@
+import { City } from '../types/city.ts';
+
+export const MockCity: City = {
+  name: 'Amsterdam',
+  location: {
+    latitude: 52.370216,
+    longitude: 4.895168,
+    zoom: 10,
+  }
+};
