11850cbad994c9b5e55eb8437ef7e5e9e20fde46
Saykho <saikho@tpu.ru>
2023-11-02 11:37:36 +0700

    write logic to display markers on the map

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,7 +1,48 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { City } from '../../types/city.ts';
+import { Icon, layerGroup, Marker } from 'leaflet';
+import { URL_MARKER_ACTIVE, URL_MARKER_DEFAULT } from '../../const/urlMarkers.ts';
+import useMap from '../../hooks/useMap.ts';
+import { OfferType } from '../../types/offer.ts';
 
-export const Map: React.FC = () => {
+type MapProps = {
+  city: City;
+  points: OfferType[];
+  selectedPoint: OfferType | undefined;
+};
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_ACTIVE,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+export const Map: React.FC<MapProps> = ({city, points, selectedPoint}) => {
   const mapRef = useRef(null);
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    if (map) {
+      const markerLayer = layerGroup().addTo(map);
+      points.forEach((point) => {
+        const marker = new Marker({
+          lat: point.location.latitude,
+          lng: point.location.longitude,
+        });
+
+        marker.setIcon(selectedPoint !== undefined && point.title ? currentCustomIcon : defaultCustomIcon).addTo(markerLayer);
+      });
+
+      return () => {
+        map.removeLayer(markerLayer);
+      };
+    }
+  }, [map, points, selectedPoint]);
 
   return (
     <div
--- src/hooks/useMap.ts
+++ src/hooks/useMap.ts
@@ -1,8 +1,8 @@
 import { MutableRefObject, useEffect, useRef, useState } from 'react';
-import { Location } from '../types/location.js';
 import { Map, TileLayer } from 'leaflet';
+import { City } from '../types/city.ts';
 
-export default function useMap(mapRef: MutableRefObject<HTMLMapElement | null>, city: Location): Map | null {
+export default function useMap(mapRef: MutableRefObject<HTMLMapElement | null>, city: City): Map | null {
   const [map, setMap] = useState<Map | null>(null);
   const isRenderedRef = useRef<boolean>(false);
 
@@ -10,10 +10,10 @@ export default function useMap(mapRef: MutableRefObject<HTMLMapElement | null>, 
     if (mapRef.current !== null && !isRenderedRef.current) {
       const instance = new Map(mapRef.current, {
         center: {
-          lat: city.latitude,
-          lng: city.longitude,
+          lat: city.location.latitude,
+          lng: city.location.longitude,
         },
-        zoom: city.zoom,
+        zoom: city.location.zoom,
       });
       const layer = new TileLayer(
         'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
