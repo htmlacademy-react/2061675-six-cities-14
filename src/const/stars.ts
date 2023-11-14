4654f9688890a125e1c4908933e266148f38eee8
ssar_say <sayhoo.saryglar@mail.ru>
2023-11-14 23:36:39 +0700

    drew a markup responding to the stars using map()

---
+++ src/const/stars.ts
@@ -0,0 +1,27 @@
+export const Stars = [
+  {
+    value: '1',
+    id: '1-stars',
+    title: 'terribly'
+  },
+  {
+    value: '2',
+    id: '2-stars',
+    title: 'badly'
+  },
+  {
+    value: '3',
+    id: '3-stars',
+    title: 'not bad'
+  },
+  {
+    value: '4',
+    id: '4-stars',
+    title: 'good'
+  },
+  {
+    value: '5',
+    id: '5-stars',
+    title: 'perfect'
+  },
+];
