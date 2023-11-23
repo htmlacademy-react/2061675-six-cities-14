8d9e4c482424e90a4c7a070ca77f377b7b6c9ee9
Saykho <sayhoo.saryglar@mail.ru>
2023-11-23 12:15:16 +0700

    fix errors

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -2,7 +2,7 @@ import { AppRoute, AuthorizationStatus } from '../../const';
 import { StateStatus } from '../../const';
 import { createReducer, createSelector, Reducer } from '@reduxjs/toolkit';
 import { requireAuthorization, setUserInfoAction } from '../actions';
-import { checkAuthAction, loginAction, logoutAction } from '../async-actions';
+import { loginAction, logoutAction } from '../async-actions';
 import { UserAuthData } from '../../types';
 
 interface AuthState {
@@ -27,22 +27,23 @@ export const authReducer: Reducer<typeof initialState> = createReducer(initialSt
       ...state,
       authorizationStatus: payload.authorizationStatus
     }))
-    .addCase(checkAuthAction.pending, (state) => {
-      state.loading = true;
-      state.status = StateStatus.loading;
-    })
-    .addCase(checkAuthAction.fulfilled, (state) => ({
-      ...state,
-      authorizationStatus: AuthorizationStatus.Auth,
-      loading: false,
-      status: StateStatus.idle,
-    }))
-    .addCase(checkAuthAction.rejected, (state) => ({
-      ...state,
-      authorizationStatus: AuthorizationStatus.NoAuth,
-      loading: false,
-      status: StateStatus.idle,
-    }))
+    // .addCase(checkAuthAction.pending, (state) => ({
+    //   ...state,
+    //   loading: true,
+    //   status: StateStatus.loading,
+    // }))
+    // .addCase(checkAuthAction.fulfilled, (state) => ({
+    //   ...state,
+    //   authorizationStatus: AuthorizationStatus.Auth,
+    //   loading: false,
+    //   status: StateStatus.idle,
+    // }))
+    // .addCase(checkAuthAction.rejected, (state) => ({
+    //   ...state,
+    //   authorizationStatus: AuthorizationStatus.NoAuth,
+    //   loading: false,
+    //   status: StateStatus.idle,
+    // }))
     .addCase(loginAction.pending, (state) => ({
       ...state,
       loading: true,
