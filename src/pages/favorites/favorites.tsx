5e94b97f9bb15defe8eb938e1bd70f401f93c5e1
Saykho <sayhoo.saryglar@mail.ru>
2023-12-01 11:08:54 +0700

    implemented correct display of favorites

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -25,47 +25,35 @@ export const Favorites: React.FC = () => {
                 <h1 className="favorites__title">Saved listing</h1>
                 <ul className="favorites__list">
                   {
-                    favoriteOffers.map((fo) => (
-                      <li className="favorites__locations-items" key={fo.id}>
-                        <div className="favorites__locations locations locations--current">
-                          <div className="locations__item">
-                            <a className="locations__item-link" href="#">
-                              <span>{fo.city.name}</span>
-                            </a>
+                    Array.from(new Set(favoriteOffers.map((fo) => fo.city.name))).map((cityName) => {
+                      const cityOffers = favoriteOffers.filter((fo) => fo.city.name === cityName);
+
+                      return (
+                        <li className="favorites__locations-items" key={cityOffers[0].id}>
+                          <div className="favorites__locations locations locations--current">
+                            <div className="locations__item">
+                              <a className="locations__item-link" href="#">
+                                <span>{cityName}</span>
+                              </a>
+                            </div>
                           </div>
-                        </div>
-                        <div className="favorites__places">
-                          <Card
-                            className="favorites__card"
-                            classNameWrapper="favorites__image-wrapper"
-                            offer={fo}
-                            classNameInfo="favorites__card-info"
-                            imgWidth="150"
-                            imgHeight="110"
-                          />
-                        </div>
-                      </li>
-                    ))
+                          <div className="favorites__places">
+                            {cityOffers.map((fo) => (
+                              <Card
+                                className="favorites__card"
+                                classNameWrapper="favorites__image-wrapper"
+                                offer={fo}
+                                classNameInfo="favorites__card-info"
+                                imgWidth="150"
+                                imgHeight="110"
+                                key={fo.id}
+                              />
+                            ))}
+                          </div>
+                        </li>
+                      );
+                    })
                   }
-                  {/*<li className="favorites__locations-items">*/}
-                  {/*  <div className="favorites__locations locations locations--current">*/}
-                  {/*    <div className="locations__item">*/}
-                  {/*      <a className="locations__item-link" href="#">*/}
-                  {/*        <span>Amsterdam</span>*/}
-                  {/*      </a>*/}
-                  {/*    </div>*/}
-                  {/*  </div>*/}
-                  {/*  <div className="favorites__places">*/}
-                  {/*    <OffersList*/}
-                  {/*      className="favorites__card"*/}
-                  {/*      classNameWrapper="favorites__image-wrapper"*/}
-                  {/*      offers={favoriteOffers}*/}
-                  {/*      classNameInfo="favorites__card-info"*/}
-                  {/*      imgWidth="150"*/}
-                  {/*      imgHeight="110"*/}
-                  {/*    />*/}
-                  {/*  </div>*/}
-                  {/*</li>*/}
                 </ul>
               </section>
             ) : <FavoriteEmpty/>
