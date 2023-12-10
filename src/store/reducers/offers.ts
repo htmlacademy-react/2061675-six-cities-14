21ea620e4ad5db1df7f2a83d193ec019c8e938de
ssar_say <sayhoo.saryglar@mail.ru>
2023-12-11 00:44:41 +0700

    add randomCity to login.tsx, defaultCity from route query, fix redirect middleware

--- src/store/reducers/offers.ts
+++ src/store/reducers/offers.ts
@@ -1,5 +1,5 @@
 import { OfferType, SelectedOffer } from '../../types';
-import { StateStatus } from '../../const';
+import { DefaultCities, StateStatus } from '../../const';
 import { createReducer, createSelector } from '@reduxjs/toolkit';
 import { fetchOffersAction, getSelectedOfferAction } from '../async-actions';
 
@@ -96,55 +96,6 @@ export const getCitiesSelector = createSelector(
   getOffersSelector,
   (offers) => {
     const cities = [...new Map(offers.map((offer) => [offer.city['name'], offer.city])).values()];
-    return cities.length ? cities : [
-      {
-        name: 'Amsterdam',
-        location: {
-          'latitude': 52.37454,
-          'longitude': 4.897976,
-          'zoom': 13
-        }
-      },
-      {
-        name: 'Paris',
-        location: {
-          'latitude': 48.85661,
-          'longitude': 2.351499,
-          'zoom': 13
-        }
-      },
-      {
-        name: 'Cologne',
-        location: {
-          'latitude': 50.938361,
-          'longitude': 6.959974,
-          'zoom': 13
-        }
-      },
-      {
-        name: 'Brussels',
-        location: {
-          'latitude': 50.846557,
-          'longitude': 4.351697,
-          'zoom': 13
-        }
-      },
-      {
-        name: 'Hamburg',
-        location: {
-          'latitude': 53.550341,
-          'longitude': 10.000654,
-          'zoom': 13
-        }
-      },
-      {
-        name: 'Dusseldorf',
-        location: {
-          'latitude': 51.225402,
-          'longitude': 6.776314,
-          'zoom': 13
-        }
-      },
-    ];
+    return cities.length ? cities : DefaultCities;
   }
 );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,5 @@ export * from './mock-component.tsx';
 export * from './mocks.ts';
 export * from './date-format.ts';
 export * from './get-rating.ts';
+export * from './capitalizeFirstLetter.ts';
 
