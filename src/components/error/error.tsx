091312490559eb15a4b206415a06e5198341c334
ssar_say <sayhoo.saryglar@mail.ru>
2023-12-10 22:22:40 +0700

    fix styles errors

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './error.module.scss';
+import './error.scss';
 
 export const Error: React.FC = () => (
   <div className='not-found'>
